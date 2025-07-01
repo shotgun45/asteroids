7f6879841d470391af821fc2aa8da838c4dbf140
shotgun45 <jipeles@yahoo.com>
2025-07-01 15:14:04 -0400

    Add asteroids

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     is_running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # clock and delta time to control the frame rate
     clock = pygame.time.Clock()
     dt = 0
 
     # groups
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    # assign groups to Player containers
+    # assign groups to containers
     Player.containers = updatables, drawables
+    Asteroid.containers = asteroids, updatables, drawables
+    AsteroidField.containers = (updatables,)
 
-    # create player (it will auto-add to the groups)
+    # create player (automatically added to groups)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # create asteroid field
+    asteroid_field = AsteroidField()
 
     while is_running:
         for event in pygame.event.get():
