0188ac0f1376fe29c6940077b43ebdd5bca6ca73
shotgun45 <jipeles@yahoo.com>
2025-07-01 14:38:46 -0400

    Create groups

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ def main():
     pygame.init()
     is_running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # clock and delta time to control the frame rate
     clock = pygame.time.Clock()
     dt = 0
 
     # groups
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
 
-    # classes in the groups
-    Player.containers = updatable, drawable
+    # assign groups to Player containers
+    Player.containers = updatables, drawables
+
+    # create player (it will auto-add to the groups)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while is_running:
         for event in pygame.event.get():
@@ -27,17 +29,18 @@ def main():
         # draw the background
         screen.fill((0, 0, 0))
 
-        # update the player
-        player.update(dt)
+        # update logic
+        updatables.update(dt)
 
-        # draw the player
-        player.draw(screen)
+        # draw everything
+        for drawable in drawables:
+            drawable.draw(screen)
 
         # update the screen
         pygame.display.flip()
 
         # limit frame rate to 60 FPS
-        dt = clock.tick(60) / 1000.0  # convert milliseconds to seconds
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
